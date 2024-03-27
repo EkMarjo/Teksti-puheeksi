f6932fd5cd4c7d6f1420278e3d8f36bcffecc00c
Marjo <93marjo.ek@gmail.com>
2024-03-27 11:45:00 +0200

    Teksti puheeksi tehtävä

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as React from 'react';
+import * as Speech from 'expo-speech';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [teksti, setTeksti] = useState('');
+
+  const muutaPuheeksi = () => {
+    Speech.speak(teksti);
+    console.log(teksti);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+      keyboardType='default'
+      style={{fontSize:18, width:200, borderColor: 'gray', borderWidth:1}}
+      onChangeText={teksti => setTeksti(teksti)}
+      value={teksti}
+      />
+      <Button title="Press to hear text" onPress={muutaPuheeksi} />
       <StatusBar style="auto" />
     </View>
   );
